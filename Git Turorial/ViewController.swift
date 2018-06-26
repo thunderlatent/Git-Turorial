ce207426285da701fd8063aac5c1336e7ef264e7
陳裕銘 <jinny1993820@yahoo.com.tw>
2018-06-26 15:51:01 +0800

    Initial Commit

---
+++ Git Turorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git Turorial
+//
+//  Created by 陳裕銘 on 2018/6/26.
+//  Copyright © 2018年 yuming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
