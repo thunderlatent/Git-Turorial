500fd02a8cdc29d75d279fca7b7f259698207bb6
陳裕銘 <jinny1993820@yahoo.com.tw>
2018-06-26 16:41:34 +0800

    I add a message to print when viewDidLoad launch.

--- Git Turorial/ViewController.swift
+++ Git Turorial/ViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+let message = "Hello Git"
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(message)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
